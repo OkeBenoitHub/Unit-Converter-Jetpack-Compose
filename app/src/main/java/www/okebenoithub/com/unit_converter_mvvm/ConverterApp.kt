ab1813662c063f75aa611610718399a6b463c06c
preslybenoit <okebenoithub@gmail.com>
2023-10-04 20:40:27 +0100

    Jetpack Compose Unit Converter

--- app/src/main/java/www/okebenoithub/com/unit_converter_mvvm/ConverterApp.kt
+++
@@ -1,7 +0,0 @@
-package www.okebenoithub.com.unit_converter_mvvm
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ConverterApp : Application()
--- app/src/main/java/www/okebenoithub/com/unit_converter_mvvm/data/Conversion.kt
+++
@@ -1,9 +0,0 @@
-package www.okebenoithub.com.unit_converter_mvvm.data
-
-data class Conversion(
-   val id : Int,
-   val description : String,
-   val convertFrom : String,
-   val convertTo : String,
-   val multiplyBy : Double
-)
